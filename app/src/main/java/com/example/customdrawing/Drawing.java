efa47f2460a8c258c7b81da1a77469303794f518
dlam15 <31551025+dlam15@users.noreply.github.com>
2021-03-03 21:35:24 -0500

    finished flower drawing

--- app/src/main/java/com/example/customdrawing/Drawing.java
+++ app/src/main/java/com/example/customdrawing/Drawing.java
@@ -2,7 +2,11 @@ package com.example.customdrawing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 
@@ -14,7 +18,7 @@ public class Drawing extends AppCompatActivity {
         setContentView(R.layout.activity_drawing);
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
-        Flower flower = new Flower(getApplicationContext());
+        Flower flower = new Flower(getApplicationContext(), Color.RED);
         layout.addView(flower);
 
         SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar2);
@@ -32,6 +36,16 @@ public class Drawing extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+            }
+        });
+
+        Button home = (Button) findViewById(R.id.button2);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Drawing.this, MainActivity.class);
+                startActivity(intent);
+                finish ();
             }
         });
     }
